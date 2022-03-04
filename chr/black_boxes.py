4941d73d69048b65cb7fc0063dde429b55f4b04b
Fernando Cela Diaz <fcela@alum.mit.edu>
2022-03-04 16:51:29 -0500

    fixed torch.tensor

--- chr/black_boxes.py
+++ chr/black_boxes.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.tensor as tensor
+#import torch.tensor as tensor
+import torch.Tensor as tensor
 from torch.utils.data import DataLoader
 
 import six
